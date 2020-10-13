cfd0e15669aaa5ee4ba619585eeb287fbfb2d8dd
Christiana Koebel <51933242+ChristianaKoebel@users.noreply.github.com>
2020-10-12 21:10:34 -0400

    Update server.R

--- server.R
+++ server.R
@@ -78,82 +78,62 @@ function(input, output){
       
       mutate(pop_growth = round((pop2 -pop1)/(pop1)*100, 2),
              inc_growth = round((inc2 -inc1)/(inc1)*100, 2)) %>%
-      
-      # mutate(latitude = case_when(Region == "Alberta" ~ 55.000000, 
-      #                             Region == "British Columbia" ~ 53.726669,
-      #                             Region == "Manitoba" ~ 56.415211,
-      #                             Region == "New Brunswick" ~ 46.498390,
-      #                             Region == "Newfoundland and Labrador" ~ 53.135509,
-      #                             Region == "Northwest Territories" ~ 62.453972,
-      #                             Region == "Nova Scotia" ~ 45.000000,
-      #                             Region == "Nunavut" ~ 63.746693,
-      #                             Region == "Ontario" ~ 50.000000,
-      #                             Region == "Prince Edward Island" ~ 46.250000,
-      #                             Region == "Quebec" ~ 53.000000,
-      #                             Region == "Saskatchewan" ~ 55.000000,
-      #                             Region == "Yukon" ~ 60.7212,
-      #                             TRUE ~ NA_real_),
-      #        longitude = case_when(Region == "Alberta" ~ -115.000000, 
-      #                              Region == "British Columbia" ~ -127.647621,
-      #                              Region == "Manitoba" ~ -98.739075,
-      #                              Region == "New Brunswick" ~ -66.159668,
-      #                              Region == "Newfoundland and Labrador" ~ -57.660435,
-      #                              Region == "Northwest Territories" ~ -114.371788,
-      #                              Region == "Nova Scotia" ~ -63.000000,
-      #                              Region == "Nunavut" ~ -68.516968,
-      #                              Region == "Ontario" ~ -85.000000,
-      #                              Region == "Prince Edward Island" ~ -63.000000,
-      #                              Region == "Quebec" ~ -70.000000,
-      #                              Region == "Saskatchewan" ~ -106.000000,
-      #                              Region == "Yukon" ~ 135.0568,
-      #                              TRUE ~ NA_real_)) %>%
+             
       merge(can_prov, ., by.x = "PRENAME", 
             by.y = "Region", duplicateGeoms = TRUE)
     
   })
   
   output$map1 <- renderLeaflet({
-    leaflet(filtered2()) %>% setView(lat = 60.55, lng = -70.98, zoom = 3) %>%
-      addProviderTiles(providers$CartoDB.Positron,
-                       
-                       options = providerTileOptions(noWrap = TRUE)) %>%
+    leaflet(filtered2()) %>%
+    
+      addTiles() %>%
+      
+      addPolygons(stroke = FALSE, smoothFactor = 0.3, weight = 2, opacity = 1, color = "white", 
+                  dashArray = "3", fillOpacity = 0.7,
+                  highlight = highlightOptions(
+                    weight = 5,
+                    color = "#666",
+                    dashArray = "",
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE),
+                  fillColor = ~colorBin(palette = c(brewer.pal(6, "Oranges")[1], brewer.pal(6, "Oranges")[6]),
+                                        domain = filtered2()$pop_growth)(pop_growth),
+                  popup = ~paste(sep = "", "<b>", "Region: ", "</b>", PRENAME, "</b><br/>", "<b>", "Population Growth Rate: ", "</b>",
+                                 pop_growth, "%", "</b><br/>", "<b>",
+                                 "Population in ", "</b>", "<b>", input$YearInput, "</b>", "<b>", ": ", "</b>", pop1, "</b><br/>", "<b>",
+                                 "Population in ", "</b>", "<b>", input$YearInput2, "</b>", "<b>", ": ", "</b>", pop2, "</b><br/>")) %>%
       
       addLegend("bottomright", pal = colorBin(palette = c(brewer.pal(6, "Oranges")[1], brewer.pal(6, "Oranges")[6]),
                                               domain = filtered2()$pop_growth),
-                values = ~pop_growth, title = "Population Growth Rate") %>%
+                values = ~pop_growth, title = "Population Growth Rate")
       
-      addCircles(lng = ~jitter(longitude, factor = 0), lat = ~jitter(latitude, factor = 0.01),
-                 
-                 fillOpacity = 0.8, radius = ~pop_growth*4000,
-                 popup = ~paste(sep = "", "<b>", "Region: ", "</b>", Region, "</b><br/>", "<b>", "Population Growth Rate: ", "</b>",
-                                pop_growth, "%", "</b><br/>", "<b>",
-                                "Population in ", "</b>", "<b>", input$YearInput, "</b>", "<b>", ": ", "</b>", pop1, "</b><br/>", "<b>",
-                                "Population in ", "</b>", "<b>", input$YearInput2, "</b>", "<b>", ": ", "</b>", pop2, "</b><br/>"), stroke = FALSE,
-                 
-                 color = ~colorBin(palette = c(brewer.pal(6, "Oranges")[1], brewer.pal(6, "Oranges")[6]),
-                                   domain = filtered2()$pop_growth)(pop_growth))
   })
     
   output$map2 <- renderLeaflet({
-    leaflet(filtered2()) %>% setView(lat = 60.55, lng = -70.98, zoom = 3) %>%
-      addProviderTiles(providers$CartoDB.Positron,
-                       
-                       options = providerTileOptions(noWrap = TRUE)) %>%
+    leaflet(filtered2()) %>%
+      
+      addTiles() %>%
+      
+      addPolygons(stroke = FALSE, smoothFactor = 0.3, weight = 2, opacity = 1, color = "white", 
+                  dashArray = "3", fillOpacity = 0.7,
+                  highlight = highlightOptions(
+                    weight = 5,
+                    color = "#666",
+                    dashArray = "",
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE),
+                  fillColor = ~colorBin(palette = c(brewer.pal(6, "Oranges")[1], brewer.pal(6, "Oranges")[6]),
+                                        domain = filtered2()$inc_growth)(inc_growth),
+                  popup = ~paste(sep = "", "<b>", "Region: ", "</b>", PRENAME, "</b><br/>", "<b>", "Income Growth Rate: ", "</b>",
+                                 inc_growth, "%", "</b><br/>", "<b>",
+                                 "Income in ", "</b>", "<b>", input$YearInput, "</b>", "<b>", ": ", "</b>", inc1, "</b><br/>", "<b>",
+                                 "Income in ", "</b>", "<b>", input$YearInput2, "</b>", "<b>", ": ", "</b>", inc2, "</b><br/>")) %>%
       
       addLegend("bottomright", pal = colorBin(palette = c(brewer.pal(6, "Oranges")[1], brewer.pal(6, "Oranges")[6]),
                                               domain = filtered2()$inc_growth),
-                values = ~inc_growth, title = "Income Growth Rate") %>%
-      
-      addCircles(lng = ~longitude, lat = ~latitude,
-                 
-                 fillOpacity = 0.8, radius = ~inc_growth*800,
-                 popup = ~paste(sep = "", "<b>", "Region: ", "</b>", Region, "</b><br/>", "<b>", "Income Growth Rate: ", "</b>",
-                                inc_growth, "%", "</b><br/>", "<b>",
-                                "Household Income in ", "</b>", "<b>", input$YearInput, "</b>", "<b>", ": ", "</b>", inc1, "</b><br/>", "<b>",
-                                "Household Income in ", "</b>", "<b>", input$YearInput2, "</b>", "<b>", ": ", "</b>", inc2, "</b><br/>"), stroke = FALSE,
-                 
-                 color = ~colorBin(palette = c(brewer.pal(6, "Oranges")[1], brewer.pal(6, "Oranges")[6]),
-                                   domain = filtered2()$inc_growth)(inc_growth))
+                values = ~inc_growth, title = "Income Growth Rate")
+    
   })
   
   output$tree <- renderHighchart({
